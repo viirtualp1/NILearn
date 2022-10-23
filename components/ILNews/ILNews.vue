c5d0cd7b0f6e0f80f6a893979e0ebc1f98964888
Nikita <zininnikita309@gmail.com>
2022-10-23 12:09:46 +0900

    IL-1

---
+++ components/ILNews/ILNews.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="articles">
+    <v-card
+      v-for="article in articles"
+      :key="article.title"
+      class="mx-auto mb-6"
+      max-width="560px"
+    >
+      <v-card-title>{{ article.title }}</v-card-title>
+      <v-card-subtitle>
+        {{ article.author }} - {{ articleDate(article.date) }}
+      </v-card-subtitle>
+
+      <v-card-text>
+        <div v-html="article.text"></div>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+import { format } from 'date-fns'
+import { ArticleData } from '@/types/article'
+
+export default defineComponent({
+  setup() {
+    const articles = ref<ArticleData[]>([
+      {
+        title: 'Сколько лет?',
+        text: 'Привет Привет Привет <br/> Привет Привет Привет ',
+        author: 'Никита',
+        rating: '4.5',
+        date: 1666494322514,
+        id: 0,
+      },
+      {
+        title: 'Сколько лет?',
+        text: 'Привет Привет Привет <br/> Привет Привет Привет ',
+        author: 'Никита',
+        rating: '4.5',
+        date: 1666494322514,
+        id: 1,
+      },
+    ])
+
+    const articleDate = function (date: number | Date) {
+      if (!date) {
+        return
+      }
+
+      return format(date, 'dd.MM.yyyy')
+    }
+
+    return {
+      articles,
+      articleDate,
+    }
+  },
+})
+</script>
--- components/ILServices/ILServices.vue
+++ components/ILServices/ILServices.vue
@@ -1,15 +1,39 @@
 <template>
-  <v-card> test </v-card>
+  <div class="il-services">
+    <v-card class="mb-3">
+      <v-list flat>
+        <v-list-item-group v-model="selectedItem" color="primary">
+          <v-list-item v-for="(service, idx) in services" :key="idx">
+            <v-list-item-icon>
+              <v-icon v-text="service.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>
+                <nuxt-link :to="service.link">{{ service.name }}</nuxt-link>
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
-import { PROJECT_NAME } from '~/types/constans'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup() {
+    const selectedItem = ref(1)
+    const services = [
+      { name: 'ILearn Тесты', icon: 'mdi-lead-pencil', link: '/tests' },
+      { name: 'ILearn Заметки', icon: 'mdi-notebook', link: '/todo' },
+      { name: 'ILearn Чаты', icon: 'mdi-chat', link: '/chat' },
+    ]
+
     return {
-      PROJECT_NAME,
+      selectedItem,
+      services,
     }
   },
 })
